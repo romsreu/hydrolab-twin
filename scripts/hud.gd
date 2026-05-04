aadf21371b2d63f72a3fe44639b5a92686852436
romsreu <romsreu@gmail.com>
2026-05-04 15:16:06 -0300

    Added fans commands

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -13,6 +13,20 @@ signal uvSupDer(toggle : bool)
 signal uvInfIzq(toggle : bool)
 signal uvInfDer(toggle : bool)
 
+signal ventLatIzq (toggle : bool)
+signal ventLatDer (toggle : bool)
+
+signal ventPostSupIzq (toggle : bool)
+signal ventPostSupMedio(toggle : bool)
+signal ventPostSupDer (toggle : bool)
+
+signal ventPostInfIzq (toggle : bool)
+signal ventPostInfMedio (toggle : bool)
+signal ventPostInfDer (toggle : bool)
+
+signal ventSupIzq (toggle : bool)
+signal ventSupDer (toggle : bool)
+
 func _ready():
 	for panel in panels.values():
 		panel.hide()
@@ -56,14 +70,47 @@ func _on_ventiladores_button_pressed() -> void:
 func _on_uv_sup_izq_toggled(toggled_on: bool) -> void:
 	uvSupIzq.emit(toggled_on)
 
-
 func _on_uv_sup_der_toggled(toggled_on: bool) -> void:
 	uvSupDer.emit(toggled_on)
-
 
 func _on_uv_inf_izq_toggled(toggled_on: bool) -> void:
 	uvInfIzq.emit(toggled_on)
 
-
 func _on_uv_inf_der_toggled(toggled_on: bool) -> void:
 	uvInfDer.emit(toggled_on)
+
+
+
+func _on_vent_lat_izq_toggled(toggled_on: bool) -> void:
+	ventLatIzq.emit(toggled_on)
+
+func _on_vent_lat_der_toggled(toggled_on: bool) -> void:
+	ventLatDer.emit(toggled_on)
+
+
+
+func _on_vent_post_sup_izq_toggled(toggled_on: bool) -> void:
+	ventPostSupIzq.emit(toggled_on)
+
+func _on_vent_post_sup_medio_toggled(toggled_on: bool) -> void:
+	ventPostSupMedio.emit(toggled_on)
+
+func _on_vent_post_sup_der_toggled(toggled_on: bool) -> void:
+	ventPostSupDer.emit(toggled_on)
+
+func _on_vent_post_inf_izq_toggled(toggled_on: bool) -> void:
+	ventPostInfIzq.emit(toggled_on)
+
+func _on_vent_post_inf_medio_toggled(toggled_on: bool) -> void:
+	ventPostInfMedio.emit(toggled_on)
+
+func _on_vent_post_inf_der_toggled(toggled_on: bool) -> void:
+	ventPostInfDer.emit(toggled_on)
+
+
+
+func _on_vent_sup_izq_toggled(toggled_on: bool) -> void:
+	ventSupIzq.emit(toggled_on)
+
+func _on_vent_sup_der_toggled(toggled_on: bool) -> void:
+	ventSupDer.emit(toggled_on)
--- scripts/main.gd
+++ scripts/main.gd
@@ -1,10 +1,16 @@
 extends Node3D
-
 @onready var uv_light_izq_sup: OmniLight3D = $PivotArmario/Armario/UVLightIzqSup
 @onready var uv_light_der_sup: OmniLight3D = $PivotArmario/Armario/UVLightDerSup
 @onready var uv_light_izq_inf: OmniLight3D = $PivotArmario/Armario/UVLightIzqInf
 @onready var uv_light_der_inf: OmniLight3D = $PivotArmario/Armario/UVLightDerInf
-
+@onready var fan_der_inf: Node3D = $PivotArmario/Armario/FanDerInf
+@onready var fan_mid_inf: Node3D = $PivotArmario/Armario/FanMidInf
+@onready var fan_izq_inf: Node3D = $PivotArmario/Armario/FanIzqInf
+@onready var fan_der_sup: Node3D = $PivotArmario/Armario/FanDerSup
+@onready var fan_mid_sup: Node3D = $PivotArmario/Armario/FanMidSup
+@onready var fan_izq_sup: Node3D = $PivotArmario/Armario/FanIzqSup
+@onready var fan_izq_lat: Node3D = $PivotArmario/Armario/FanIzqLat
+@onready var fan_der_lat: Node3D = $PivotArmario/Armario/FanDerLat
 
 func _on_hud_uv_sup_izq(toggle: bool) -> void:
 	if toggle:
@@ -12,13 +18,11 @@ func _on_hud_uv_sup_izq(toggle: bool) -> void:
 	else:
 		uv_light_izq_sup.hide()
 
-
 func _on_hud_uv_sup_der(toggle: bool) -> void:
 	if toggle:
 		uv_light_der_sup.show()
 	else:
 		uv_light_der_sup.hide()
-
 
 func _on_hud_uv_inf_izq(toggle: bool) -> void:
 	if toggle:
@@ -26,9 +30,68 @@ func _on_hud_uv_inf_izq(toggle: bool) -> void:
 	else:
 		uv_light_izq_inf.hide()
 
-
 func _on_hud_uv_inf_der(toggle: bool) -> void:
 	if toggle:
 		uv_light_der_inf.show()
 	else:
 		uv_light_der_inf.hide()
+
+func _on_hud_vent_lat_der(toggle: bool) -> void:
+	if toggle:
+		fan_der_lat.fan_on()
+	else:
+		fan_der_lat.fan_off()
+
+func _on_hud_vent_lat_izq(toggle: bool) -> void:
+	if toggle:
+		fan_izq_lat.fan_on()
+	else:
+		fan_izq_lat.fan_off()
+
+func _on_hud_vent_post_inf_der(toggle: bool) -> void:
+	if toggle:
+		fan_der_inf.fan_on()
+	else:
+		fan_der_inf.fan_off()
+
+func _on_hud_vent_post_inf_izq(toggle: bool) -> void:
+	if toggle:
+		fan_izq_inf.fan_on()
+	else:
+		fan_izq_inf.fan_off()
+
+func _on_hud_vent_post_inf_medio(toggle: bool) -> void:
+	if toggle:
+		fan_mid_inf.fan_on()
+	else:
+		fan_mid_inf.fan_off()
+
+func _on_hud_vent_post_sup_der(toggle: bool) -> void:
+	if toggle:
+		fan_der_sup.fan_on()
+	else:
+		fan_der_sup.fan_off()
+
+func _on_hud_vent_post_sup_izq(toggle: bool) -> void:
+	if toggle:
+		fan_izq_sup.fan_on()
+	else:
+		fan_izq_sup.fan_off()
+
+func _on_hud_vent_post_sup_medio(toggle: bool) -> void:
+	if toggle:
+		fan_mid_sup.fan_on()
+	else:
+		fan_mid_sup.fan_off()
+
+#func _on_hud_vent_sup_der(toggle: bool) -> void:
+	#if toggle:
+		#fan_der_sup.fan_on()
+	#else:
+		#fan_der_sup.fan_off()
+#
+#func _on_hud_vent_sup_izq(toggle: bool) -> void:
+	#if toggle:
+		#fan_izq_sup.fan_on()
+	#else:
+		#fan_izq_sup.fan_off()
